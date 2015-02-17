16d70c28a9dae0a1399f78198847bece86a0eaed
funkake <im.nevu@gmail.com>
2015-02-17 08:38:49 +0100

    Added a default constructor in the humanoid class

--- src/Humanoid.java
+++ src/Humanoid.java
@@ -8,17 +8,20 @@
  *
  * @author Stein
  */
-public class Humanoid extends Agent{
+public abstract class Humanoid extends Agent{
     
     private int strength;
     private int stamina;
     private int luck;
     private int age;
     private int hunger;
-    
-    
-    
-    
 
-    
+    protected Humanoid(Field field, Location location, int strength, int stamina, int luck, int age, int hunger) {
+        super(field, location);
+        this.strength = strength;
+        this.stamina = stamina;
+        this.luck = luck;
+        this.age = age;
+        this.hunger = hunger;
+    }
 }
