c3c5e388e335554e330665398e161709fd82c4cf
Stein Sæter <steinsater@gmail.com>
2015-02-17 08:26:55 +0100

    allfiles, lol

---
+++ src/Humanoid.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Stein
+ */
+public class Humanoid extends Agent{
+    
+    private int strength;
+    private int stamina;
+    private int luck;
+    private int age;
+    private int hunger;
+    
+    
+    
+    
+
+    
+}
